156e872cba805121c4fa77832f75bc06072891c7
Ganning Xu <ganningxu@gmail.com>
2021-12-14 19:38:02 -0500

    deploy pls

--- components/Volunteering/VolunteerCard.jsx
+++ components/Volunteering/VolunteerCard.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Text, Link, SimpleGrid, Image } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 export const VolunteerCard = (props) => (
     <Box bg="black" color="white" maxW="300px" align="center" mx="auto" p="3" className="border-solid border-2 border-light-blue rounded-md">
         <Text fontSize='2xl' fontWeight={800} align="center" className="text-light-purple">{props.position}</Text>
