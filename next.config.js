36f83f9b036015b95f51ed3f4eb0517b6e35958d
Ganning Xu <ganningxu@gmail.com>
2022-01-04 13:42:39 -0500

    add env in next.config

--- next.config.js
+++ next.config.js
@@ -13,5 +13,10 @@ module.exports = (phase, { defaultConfig }) => {
       }
       return config
     },
+    env: {
+      NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_CLIENT_KEY: process.env.NEXT_PUBLIC_SUPABASE_CLIENT_KEY,
+      NEXT_PUBLIC_SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
+    }
   }
 }
