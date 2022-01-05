3ebef352f48e8e276eec8b183df25266de502d62
Ganning Xu <ganningxu@gmail.com>
2022-01-04 22:14:05 -0500

    add award and update blog

--- pages/api/views/getBlogViews.js
+++ pages/api/views/getBlogViews.js
@@ -9,7 +9,7 @@ const fetcher = async (input) => {
 const PageViews = (slug) => {
     const { data } = useSWR(`/api/views/${slug}`, fetcher);
 
-    return data?.total ? `${data.total} views` : `–––`;
+    return data?.total ? `${data.total} views` : ``;
 };
 
 export default PageViews;
