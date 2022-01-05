df87c09d82441dd5e7c61fc83c013121e15a965b
Ganning Xu <ganningxu@gmail.com>
2022-01-05 15:14:47 -0500

    try removing hello

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
-module.exports = (phase, { defaultConfig }) => {
-  return {
-    ...defaultConfig,
+module.exports = {
 
-    webpack: (config) => {
-      config.resolve = {
-        ...config.resolve,
-        fallback: {
-          "fs": false,
-          "path": false,
-          "os": false,
-        }
-      }
-      return config
-    },
-    env: {
-      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-      NEXT_PUBLIC_SUPABASE_CLIENT_KEY: process.env.NEXT_PUBLIC_SUPABASE_CLIENT_KEY,
-      SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
-    },
-    pageExtensions: ['js', 'jsx'],
-  }
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_CLIENT_KEY: process.env.NEXT_PUBLIC_SUPABASE_CLIENT_KEY,
+    SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
+  },
+  pageExtensions: ['js', 'jsx'],
+  target: 'serverless'
 }
