28527b718f686890d37d7ba3ed9d44523b2a440e
Ganning Xu <ganningxu@gmail.com>
2022-01-15 13:31:36 -0500

    update imports

--- components/Volunteering/index.jsx
+++ components/Volunteering/index.jsx
@@ -1,13 +1,16 @@
-import { Box, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Box, Heading, SimpleGrid, Text } from '@chakra-ui/react'
 import VolunteeringList from "./volunteering.json"
 import { VolunteerCard } from './VolunteerCard'
 export const Volunteering = () => {
 
     return (
         <Box color="white" mt="8">
-            <Heading fontSize="5xl" fontWeight="bold" mt="8" color="blue.light" mb="1">
+            <Heading fontSize="5xl" fontWeight="bold" mt="8" color="blue.light">
                 volunteering
             </Heading>
+
+            <Text my="2" fontWeight="semibold" fontSize="lg" fontStyle="italic">(210+ hours)</Text>
+
             <SimpleGrid minChildWidth='300px' spacingX='40px' spacingY="20px">
                 {
                     VolunteeringList.map((volunteer, i) => (
