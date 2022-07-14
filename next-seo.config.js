9819018af9053e7be6023ba8d8b3f4f8ed31804b
Ganning Xu <ganningxu@gmail.com>
2022-07-13 21:43:13 -0400

    update description

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,24 +1,24 @@
-const title = 'Ganning Xu - Developer, Student, Aviation Enthusiast.'
+const title = "Ganning Xu - Developer, Student, Aviation Enthusiast.";
 const description =
-    'Personal website for Ganning Xu, a high school junior at the North Carolina School of Science and Mathematics.'
+  "Personal website for Ganning Xu, a high school senior at the North Carolina School of Science and Mathematics.";
 
 const SEO = {
+  title,
+  description,
+  canonical: "https://www.ganning.me",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://www.ganning.me",
     title,
     description,
-    canonical: 'https://www.ganning.me',
-    openGraph: {
-        type: 'website',
-        locale: 'en_IE',
-        url: 'https://www.ganning.me',
-        title,
-        description,
-        images: [
-            {
-                url: 'https://benjamincarlson.io/images/favicon.png',
-                alt: title
-            }
-        ]
-    }
-}
+    images: [
+      {
+        url: "https://benjamincarlson.io/images/favicon.png",
+        alt: title,
+      },
+    ],
+  },
+};
 
-export default SEO
+export default SEO;
