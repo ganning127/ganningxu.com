87bb8fcec691440b07645430b4e513e062cdeaa7
ganning127 <ganningxu@gmail.com>
2024-05-07 18:47:30 +0800

    add scrollbar styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,42 @@
-import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from '../theme'
-import React from 'react'
-import { useColorMode } from '@chakra-ui/react'
-import { MDXProvider } from '@mdx-js/react'
-import { Global, css } from '@emotion/react'
-import { DefaultSeo } from 'next-seo'
-import SEO from '../next-seo.config'
-import { prismLightTheme, prismDarkTheme } from '../styles/prism'
-import MDXComponents from '../components/MDXComponents'
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../theme';
+import React from 'react';
+import { useColorMode } from '@chakra-ui/react';
+import { MDXProvider } from '@mdx-js/react';
+import { Global, css } from '@emotion/react';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
+import { prismLightTheme, prismDarkTheme } from '../styles/prism';
+import MDXComponents from '../components/MDXComponents';
 
-const GlobalStyle = ({ children }) => {
-  let { colorMode } = useColorMode()
+const GlobalStyle = ({ children }) =>
+{
+  let { colorMode } = useColorMode();
 
+  let scrollbarStyles = ``;
 
+  if (colorMode === 'dark')
+  {
+    scrollbarStyles = `
+      
+      /* width */
+      ::-webkit-scrollbar {
+        width: 10px;
+      }
+
+      /* Track */
+      ::-webkit-scrollbar-track {
+        background: black;
+      }
+      
+      /* Handle */
+      ::-webkit-scrollbar-thumb {
+        background: #262729;
+        border-radius: 10px;
+      }
+    `;
+  }
   return (
     <>
       <Global
@@ -29,15 +52,20 @@ const GlobalStyle = ({ children }) => {
             min-height: 100vh;
             background: ${colorMode === 'light' ? 'white' : '#15161a'};
           }
+
+          ${scrollbarStyles}
+
+          
         `}
       />
       {children}
     </>
-  )
-}
+  );
+};
 
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps })
+{
   return (
     <ChakraProvider theme={theme}>
       <MDXProvider components={MDXComponents}>
@@ -48,6 +76,6 @@ function MyApp({ Component, pageProps }) {
         </GlobalStyle>
       </MDXProvider>
     </ChakraProvider >
-  )
+  );
 }
-export default MyApp
+export default MyApp;
